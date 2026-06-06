bb7e9eb574f497c09e4b8a286c85e97d070a98e8
CHEEMS <2126340634@qq.com>
2026-06-06 21:41:01 +0800

    update model

--- src/config.ts
+++ src/config.ts
@@ -576,8 +576,8 @@ export const permalinkConfig: PermalinkConfig = {
 }
 
 export const expressiveCodeConfig: ExpressiveCodeConfig = {
-	/*注意：某些样式（如背景颜色）已被覆盖，请参阅 astro.config.mjs 文件。
-                                                                                                                          请选择深色主题，因为此博客主题目前仅支持深色背景*/
+	// 注意：某些样式（如背景颜色）已被覆盖，请参阅 astro.config.mjs 文件。
+	// 请选择深色主题，因为此博客主题目前仅支持深色背景
 	theme: 'github-dark',
 
 	// 是否在主题切换时隐藏代码块以避免卡顿问题
@@ -844,7 +844,7 @@ export const pioConfig: import('./types/config').PioConfig = {
 	// 禁用看板娘以提升性能
 	enable: true,
 
-	models: ['/pio/models/pio/model.json'],
+	models: ['/pio/models/kp31/model.json'],
 
 	// 模型位置
 	position: 'left',
